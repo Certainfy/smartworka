2ce4b76d22e008b2342c22706ba6a78fc596dae6
Amodu Kehinde <amodukehindee@gmail.com>
2024-06-16 12:35:32 +0100

    WIP:final landing page🚧

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,15 +1,23 @@
+'use client'
 import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { MoveRight } from "lucide-react";
+import { MoveRight, Menu,X} from "lucide-react";
 
 export default function NavBar() {
+
+  const [isToggle, setIsToggle] = React.useState(false);
+
+  const onToggle = () => {
+    setIsToggle(!isToggle);
+  };
+
   return (
     <div>
       <nav className="flex flex-row justify-between items-center py-6 px-6">
         <div className="flex gap-12 items-center">
           <img src="/Smartworka.svg" alt="logo" />
-          <ul className="space-x-4 text-gray-400 text-base">
+          <ul className="space-x-4 text-gray-400 text-base hidden lg:block">
             <Link href="#about" className="text-gray-400 hover:text-blue-600">
               About
             </Link>
@@ -27,7 +35,7 @@ export default function NavBar() {
             </Link>
           </ul>
         </div>
-        <div className="space-x-3">
+        <div className="space-x-3 hidden lg:block">
           <Button className="border-0" variant="outline">
             Login
           </Button>
@@ -35,6 +43,46 @@ export default function NavBar() {
             Get Started <MoveRight className="ml-2 h-4 w-4" />
           </Button>
         </div>
+        <button onClick={onToggle} className="lg:hidden">
+            <Menu className="text-2xl" />
+          </button>
+      </nav>
+
+      <nav className={`bg-[#051537] h-full absolute w-full z-40 rounded-md lg:hidden px-2 py-4 bottom-4 ${isToggle ? 'block' : 'hidden'}`}>
+         <div className="flex flex-row justify-between">
+         <img src="/Smartworka.svg" alt="logo" />
+         <button className="p-4" onClick={onToggle}>
+            <X className="text-2xl text-white" />  
+          </button>
+         </div>
+         
+          <ul className="space-x-4 flex lg:hidden flex-col gap-8 items-center text-white text-lg py-12">
+            <Link href="#about" className="text-gray-400 hover:text-blue-600">
+              About
+            </Link>
+            <Link
+              href="#features"
+              className="text-gray-400 hover:text-blue-600"
+            >
+              Features
+            </Link>
+            <Link href="#works" className="text-gray-400 hover:text-blue-600">
+             How it Works
+            </Link>
+            <Link href="#contact" className="text-gray-400 hover:text-blue-600">
+              Contact
+            </Link>
+          </ul>
+        
+        <div className="space-x-3 hidden lg:block">
+          <Button className="border-0" variant="outline">
+            Login
+          </Button>
+          <Button className="text-white rounded-full bg-blue-600 hover:bg-blue-800">
+            Get Started <MoveRight className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
+        
       </nav>
     </div>
   );
--- src/constant/tabsData.tsx
+++ src/constant/tabsData.tsx
@@ -3,8 +3,8 @@ export const tabs = [
       id: 0,
       logo: "/img-1.png",
       content: (
-        <div className="flex justify-center items-center gap-24">
-          <div className="flex gap-4">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-24">
+          <div className="flex flex-col items-center lg:flex-row gap-4">
             <div>
               <img
                 src="/reveiw-1.png"
@@ -13,12 +13,12 @@ export const tabs = [
               />
             </div>
             <div>
-              <p className="text-[18px] text-white text-center md:text-left">
+              <p className="text-[14px] lg:text-[18px] text-white text-center md:text-left">
                 “Software for accounting firms is notoriously <br /> outdated,
                 slow, and hard to use. Smartworka is <br /> different – it’s a
                 more streamlined, cloud-first approach.”
               </p>
-              <p className="mt-4 text-sm font-semibold text-white text-center md:text-left">
+              <p className="mt-4 text-xs lg:text-sm font-semibold text-white text-center md:text-left">
                 Kate Williams
                 <br />
                 Partner, Maxwell Locke & Ritter
@@ -28,7 +28,7 @@ export const tabs = [
           <div className="text-white">
             <p className="text-6xl font-bold">5X</p>
             <p className="text-lg">Business Growth</p>
-            <p className="w-full">
+            <p className="w-full text-sm lg:text-md">
               Smartworka innovations and
               <br /> efficiencies helped Maxwell
               <br /> Locke & Ritter grow their
